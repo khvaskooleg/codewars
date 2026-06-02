5d10d50601026140386b1bb6662682bda31ef968
khvaskooleg <khvaskooleg@gmail.com>
2026-06-03 02:52:54 +0300

    multiply-1

---
+++ 8kyu/multiply.js
@@ -0,0 +1,12 @@
+// Description:
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//   a * b
+// }
+
+function multiply(a, b){
+  let c = a * b
+  return c
+}
+
