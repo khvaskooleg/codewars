15edbaf9b044e25b73fb56976d96eb90e81d06a6
khvaskooleg <khvaskooleg@gmail.com>
2026-06-03 03:55:40 +0300

    grasshopper-Terminal-game-move-function-1

---
+++ 8kyu/Grasshopper-Terminal-game-move-function.js
@@ -0,0 +1,18 @@
+// Description:
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+//   let roll = Math.floor(Math.random()*6 + 1);
+//   console.log(roll);
+
+function move (position, roll) {
+  return newPosition = position + roll*2;
+}
+
+move(7, 3)
+console.log(newPosition);
