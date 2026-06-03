cec17b38a810e78f48382d1820a313bdc9e14a56
khvaskooleg <khvaskooleg@gmail.com>
2026-06-03 03:16:12 +0300

    convert-boolean-to-strings-1

---
+++ 8kyu/convert-boolean-values-to-strings.js
@@ -0,0 +1,17 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// function boolToWord( bool ){
+//   //...
+// }
+
+// function boolToWord( bool ){
+//   if ( bool ) {
+//     return "Yes"
+//   } else {
+//     return "No"
+//   }
+// }
+
+function boolToWord( bool ){
+  return bool ? "Yes" : "No";
+}
