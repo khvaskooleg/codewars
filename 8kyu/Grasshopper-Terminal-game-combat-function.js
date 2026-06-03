4594bea14e00c442c6a88fb6b17d085cdb7e9915
khvaskooleg <khvaskooleg@gmail.com>
2026-06-03 03:37:36 +0300

    grasshopper-Terminal-game-combat-function-1

---
+++ 8kyu/Grasshopper-Terminal-game-combat-function.js
@@ -0,0 +1,15 @@
+// Create a combat function that takes the player's current health and the amount
+//  of damage received, and returns the player's new health. Health can't be less than 0.
+
+// function combat(health, damage) {
+//   // Write your code here
+// }
+
+
+function combat(health, damage) {
+  if ((health - damage) >= 0 ) {
+    return (health - damage)
+  } else {
+    return 0
+  }
+}
